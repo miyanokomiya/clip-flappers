68f78f5ff849adee7b7744136d62ed6427e0f5c8
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-11-26 00:52:58 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,61 @@
-import { Size, fileToBase64, base64ToImage } from 'okanvas'
+import {
+  Size,
+  fileToBase64,
+  base64ToImage,
+  getCentralizedViewBox,
+} from 'okanvas'
 
 const SVG_URL = 'http://www.w3.org/2000/svg'
 
-const c = (tag: string) => document.createElement(tag)
-const cs = (tag: string) => document.createElementNS(SVG_URL, tag)
 const g = (el: string) => document.getElementById(el)
+const appendChild = ($el: Element, $child: Element) => $el.appendChild($child)
+const setAttribute = ($el: Element, name: string, value: string) =>
+  $el.setAttribute(name, value)
+
+function createHTMLElement(
+  tag: string,
+  attributes: { [key: string]: string | number } | null = null,
+  children: Element[] | string = []
+): HTMLElement {
+  const $el = document.createElement(tag)
+  return createElement($el, attributes, children)
+}
+
+function createSVGElement(
+  tag: string,
+  attributes: { [key: string]: string | number } | null = null,
+  children: Element[] | string = []
+): SVGElement {
+  const $el = document.createElementNS(SVG_URL, tag)
+  return createElement($el, attributes, children)
+}
+
+function createElement<T extends Element>(
+  $el: T,
+  attributes: { [key: string]: string | number } | null = null,
+  children: Element[] | string = []
+): T {
+  for (const key in attributes) {
+    setAttribute($el, key, attributes[key].toString())
+  }
+  if (Array.isArray(children)) {
+    for (let i = 0; i < children.length; i++) {
+      $el.appendChild(children[i])
+    }
+  } else {
+    $el.textContent = children
+  }
+  return $el
+}
+
+function setStyles(el: Element, styles: { [key: string]: string }) {
+  el.setAttribute(
+    'style',
+    Object.keys(styles)
+      .map((key) => `${key}:${styles[key]};`)
+      .join('')
+  )
+}
 
 interface ErrorMessages {
   invalidImageFile: string
@@ -12,16 +63,8 @@ interface ErrorMessages {
 
 interface Props {
   viewSize?: Size
+  clipSize?: Size
   errorMessages?: ErrorMessages
-}
-
-function setStyles(el: HTMLElement, styles: { [key: string]: string }) {
-  el.setAttribute(
-    'style',
-    Object.keys(styles)
-      .map((key) => `${key}:${styles[key]};`)
-      .join('')
-  )
 }
 
 export default class ClipFlappers {
@@ -33,6 +76,7 @@ export default class ClipFlappers {
   }
 
   private viewSize: Size = { width: 124, height: 124 }
+  private clipSize: Size = { width: 124, height: 124 }
 
   constructor(el: string | Element, props?: Props) {
     if (el instanceof Element) {
@@ -47,58 +91,111 @@ export default class ClipFlappers {
 
     if (props?.errorMessages) this.errorMessages = props.errorMessages
     if (props?.viewSize) this.viewSize = props.viewSize
+    if (props?.clipSize) this.clipSize = props.clipSize
 
     this.render()
   }
 
   private render() {
-    const $root = c('div')
+    const $root = createHTMLElement('div', null, [])
 
-    const $fileInput = c('input')
-    $fileInput.setAttribute('type', 'file')
-    $fileInput.setAttribute('accept', 'image/*')
+    const $fileInput = createHTMLElement('input', {
+      type: 'file',
+      accept: 'image/*',
+    })
     $fileInput.oninput = (e) => this.onInputFile(e)
-    $root.appendChild($fileInput)
 
-    const $button = c('button')
-    $button.innerText = 'Select'
+    const $button = createHTMLElement('button', null, 'Select')
     $button.onclick = () => $fileInput.click()
-    $root.appendChild($button)
 
-    const $svgWrapper = c('div')
+    const $svgWrapper = createHTMLElement('div')
     setStyles($svgWrapper, {
       padding: '8px',
       border: '1px solid #000',
       backgroundColor: '#ccc',
       overflow: 'hidden',
     })
-    $svgWrapper.style.width = `${this.viewSize.width}px`
-    $svgWrapper.style.height = `${this.viewSize.height}px`
+    setStyles($svgWrapper, {
+      width: `${this.viewSize.width}px`,
+      height: `${this.viewSize.height}px`,
+    })
 
-    const $svg = cs('svg')
-    $svg.setAttribute('xmlns', SVG_URL)
-    $svg.setAttribute('viewBox', '0 0 100 100')
-    $svg.style.width = '100%'
-    $svg.style.height = '100%'
+    const $svg = createSVGElement('svg', {
+      xmlns: 'http://www.w3.org/2000/svg',
+      viewBox: '0 0 100 100',
+    })
+    setStyles($svg, {
+      width: '100%',
+      height: '100%',
+    })
 
-    $svgWrapper.appendChild($svg)
-    $root.appendChild($svgWrapper)
-
-    this.$el.appendChild($root)
+    appendChild($svgWrapper, $svg)
+    appendChild($root, $fileInput)
+    appendChild($root, $button)
+    appendChild($root, $svgWrapper)
+    appendChild(this.$el, $root)
   }
 
   private async updateImage() {
     const image = await base64ToImage(this.base64)
-    const $image = cs('image')
-    $image.setAttribute('href', this.base64)
-    $image.setAttribute('x', '0')
-    $image.setAttribute('y', '0')
-    $image.setAttribute('width', `${image.width}`)
-    $image.setAttribute('height', `${image.height}`)
+    const $image = createSVGElement('image', {
+      x: 0,
+      y: 0,
+      width: image.width,
+      height: image.height,
+      href: this.base64,
+    })
 
-    const $svg = this.$el.getElementsByTagName('svg')
-    $svg[0]?.setAttribute('viewBox', `0 0 ${image.width} ${image.height}`)
-    $svg[0]?.appendChild($image)
+    const $svg = this.$el.getElementsByTagName('svg')[0]!
+    $svg.innerHTML = ''
+    setAttribute($svg, 'viewBox', `0 0 ${image.width} ${image.height}`)
+    appendChild($svg, $image)
+
+    const scale = 1
+    const clipRect = getCentralizedViewBox(this.clipSize, image)
+    const $clipRect = createSVGElement(
+      'g',
+      {
+        transform: `translate(${clipRect.x}, ${clipRect.y})`,
+      },
+      [
+        createSVGElement('rect', {
+          x: 0,
+          y: 0,
+          width: clipRect.width,
+          height: clipRect.height,
+          fill: 'none',
+          stroke: 'red',
+          'stroke-width': 4 * scale,
+        }),
+        createSVGElement('g', null, [
+          createSVGElement('circle', {
+            cx: 0,
+            cy: 0,
+            r: 8 * scale,
+            fill: 'red',
+            stroke: 'none',
+          }),
+        ]),
+        createSVGElement(
+          'g',
+          {
+            transform: `translate(${clipRect.width}, ${clipRect.height})`,
+          },
+          [
+            createSVGElement('circle', {
+              cx: 0,
+              cy: 0,
+              r: 8 * scale,
+              fill: 'red',
+              stroke: 'none',
+            }),
+          ]
+        ),
+      ]
+    )
+
+    appendChild($svg, $clipRect)
   }
 
   private async onInputFile(event: any) {
