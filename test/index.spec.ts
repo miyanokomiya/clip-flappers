8b5af66a2d003da91874360b02438c0381a71baa
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-11-26 21:59:57 +0900

    fix test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,19 @@
-import target from '../src/index'
+import Target from '../src/index'
+import { image200x100 } from './assets'
 
 describe('index', () => {
-  it('TODO', () => {
-    expect(target()).toBe(1)
+  describe('snapshot', () => {
+    it('default', () => {
+      const $el = document.createElement('div')
+      const app = new Target($el) as any
+      expect(app.$el).toMatchSnapshot()
+    })
+    it('image loaded', async () => {
+      const $el = document.createElement('div')
+      const app = new Target($el) as any
+      app.base64 = image200x100
+      await app.updateImage()
+      expect(app.$el).toMatchSnapshot()
+    })
   })
 })
